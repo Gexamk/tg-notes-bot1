a5404487264cef0eceffe8d361df8db2243abd52
Gexamk <genadii.sobolev@gmail.com>
2025-04-22 08:58:14 +0300

    2 US added to req + sql schema has been updated

--- schema.sql
+++ schema.sql
@@ -1,7 +1,38 @@
-CREATE TABLE media_notes (
+CREATE TABLE IF NOT EXISTS media_notes (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     author TEXT,
     category TEXT CHECK (category IN ('Cinema', 'Book', 'Song')) NOT NULL,
     status TEXT CHECK (status IN ('planned', 'done')) DEFAULT 'planned'
 );
+
+
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns 
+        WHERE table_name='media_notes' AND column_name='created_at'
+    ) THEN
+        ALTER TABLE media_notes
+        ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+    END IF;
+END
+$$;
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    telegram_id BIGINT UNIQUE NOT NULL
+);
+
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name = 'media_notes' AND column_name = 'user_id'
+    ) THEN
+        ALTER TABLE media_notes
+        ADD COLUMN user_id INTEGER REFERENCES users(id);
+    END IF;
+END
+$$;

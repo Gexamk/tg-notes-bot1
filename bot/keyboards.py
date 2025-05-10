2e052e4a9477a6ce67d14d366517e939043d51fa
Gexamk <genadii.sobolev@gmail.com>
2025-05-10 13:38:17 +0300

    aa new to view marckup

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -8,5 +8,5 @@ MAIN_MARKUP = ReplyKeyboardMarkup(MAIN_KEYBOARD, resize_keyboard=True)
 CATEGORY_KEYBOARD = [["🎬 Cinema", "📚 Book"], ["🛒 Shop","🗒 Note"], ["🔙 Back"]]
 CATEGORY_MARKUP = ReplyKeyboardMarkup(CATEGORY_KEYBOARD, resize_keyboard=True)
 
-VIEW_KEYBOARD = [["✅ Mark|Unmark", "🗑 Delete"], ["🔙 Back"]]
+VIEW_KEYBOARD = [["➕ New","✅ Mark|Unmark", "🗑 Delete"], ["🔙 Back"]]
 VIEW_MARKUP = ReplyKeyboardMarkup(VIEW_KEYBOARD, resize_keyboard=True)
